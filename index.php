a5d6e59bf53c6a42942e7b2a59681526cecb440b
whyadtya <71758931+whyadtya@users.noreply.github.com>
2020-09-24 15:59:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>Tugas Cloud</title>
 </head>
 <body>
  <h1>Fasilitas UIN Syarif Hidayatullah Jakarta </h1> <align "justify center"
- <h2>Student Center <h2>
+  <center>                                                       
  <img src="Student Center.jpg" alt="SC" width="800" height="500">
+ </center>
  <font size="4"
 <p>Nama saya wahyu aditya Aku cinta dia </p>
 
