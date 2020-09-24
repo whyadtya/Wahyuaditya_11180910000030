51fd9693b1a808e9133f2d92766a6439bf0354e7
whyadtya <71758931+whyadtya@users.noreply.github.com>
2020-09-24 15:48:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 <title>Page Title</title>
 </head>
 <body>
- <img src="Carenang.jpg" alt="Pemandangan Sunset" width="500" height="250">
+ <h1>Fasilitas UIN Syarif Hidayatullah Jakarta </h1> <align "justify center"
+ <h2>Student Center <h2>
+ <img src="Student Center.jpg" alt="SC" width="800" height="500">
  <font size="4"
-<h1>PEMANDANGAN</h1> <align "justify center"
-<p>Nama saya wahyu aditya</p>
+<p>Nama saya wahyu aditya Aku cinta dia </p>
 
 </body>
 </html>
