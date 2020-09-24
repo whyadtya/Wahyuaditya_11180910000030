efabfb6c8c8952ac5307e374f08cbb8aaf8ad38e
whyadtya <71758931+whyadtya@users.noreply.github.com>
2020-09-24 16:01:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
  <h1>Fasilitas UIN Syarif Hidayatullah Jakarta </h1> <align "justify center"
   <center>                                                       
- <img src="Student Center.jpg" alt="SC" width="800" height="500">
+ <img src="StudentCenter.jpg" alt="Setudent Center" width="800" height="500">
  </center>
  <font size="4"
 <p>Nama saya wahyu aditya Aku cinta dia </p>
