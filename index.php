4a6e884cf5ab962a77b0df83755df5f5b8f3ea97
whyadtya <71758931+whyadtya@users.noreply.github.com>
2020-09-25 20:49:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
     <div class="col-sm-4">
       <h2>Tentang Saya</h2>
       <h5>Photo of me:</h5>
-      <img src="whyadtya.JPG" width="350px" height="197px" style="list-style:none; margin-left:-40px>
+      <img src="whyadtya.JPG" width="350px" height="200px" style="list-style:none; margin-left:-40px>
       <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
       <h3>Some Links</h3>
       <p>Lorem ipsum dolor sit ame.</p>
@@ -62,13 +62,13 @@
       <h5>Jakarta, Sep 15, 2020</h5>
       <img src="Perpustakaan Umum.jpg" alt"Perpustakaan">
       <p>PERPUSTAKAAN UMUM</p>
-      <font size="6", face="Times New Roman"> Perpustakaan Umum merupakan salah satu fasilitas yang dimiliki oleh UIN Syarif Hidayatullah Jakarata. Perpustakaan menjadi tempat yang sangat diminati dan sangat sering sekali di kunjungi oleh para mahasiswa, hal itu tak lepas dari kenyamanan sekaligus fasilitas yang di miliki oleh perpustakaan itu sendiri. Dengan melihat antusias dari para mahasiswa yang sangat sering sekali mengunjungi perpustakaan, baik itu hanya untuk sekedar beristirahat sambil membaca-baca buku ataupun berdiskusi yang dilakukan oleh sekumpulan mahasiswa yang membicarakan baik itu seputar perkuliahan ataupun yang lainnya. Hal itu lah yang tentunya menjadi nilai menarik bagi saya untuk mengambil konten menarik dari salah satu fasilitas yang ada di UIN Syarif Hidayatullah Jakarta, maka yang saya rasa lebih masuk dari kriteria tersebut dan relevan dengan keadaan sekarang, oleh karenanya lah saya memilih Perpustakaan Umum sebagai fasilitas menarik yang ada di UIN Syarif Hidayatullah Jakarta.  .</p>
+      <font class="justify" size="4", face="Times New Roman"> Perpustakaan Umum merupakan salah satu fasilitas yang dimiliki oleh UIN Syarif Hidayatullah Jakarata. Perpustakaan menjadi tempat yang sangat diminati dan sangat sering sekali di kunjungi oleh para mahasiswa, hal itu tak lepas dari kenyamanan sekaligus fasilitas yang di miliki oleh perpustakaan itu sendiri. Dengan melihat antusias dari para mahasiswa yang sangat sering sekali mengunjungi perpustakaan, baik itu hanya untuk sekedar beristirahat sambil membaca-baca buku ataupun berdiskusi yang dilakukan oleh sekumpulan mahasiswa yang membicarakan baik itu seputar perkuliahan ataupun yang lainnya. Hal itu lah yang tentunya menjadi nilai menarik bagi saya untuk mengambil konten menarik dari salah satu fasilitas yang ada di UIN Syarif Hidayatullah Jakarta, maka yang saya rasa lebih masuk dari kriteria tersebut dan relevan dengan keadaan sekarang, oleh karenanya lah saya memilih Perpustakaan Umum sebagai fasilitas menarik yang ada di UIN Syarif Hidayatullah Jakarta.  .</p>
       <br>
       <h2>Study From Home</h2>
       <h5>Jakarat, Sep 25, 2020</h5>
        <img src="Suasana SFH.jpg">
       <p>Study From Home</p>
-      <font size="6", face="Times New Roman"> Foto tersebut merupakan suatu gambaran saya selama melakukan Study From Home, Adapun yang saya lakukan di sela-sela waktu ketika sedang mengerjakan tugas ataupun belajar di rumah yaitu mendengarkan lagu dan segelas kopi yang setia menemani saya ketika sedang kuliah daring ataupun sedang nugas. Kedua hal tersebut sepertinya sudah menjadi kebiasaan bagi saya dalam mensiasati rasa jenuh ketika saya hanya diam dirumah, selain itu saya juga kadang menonton beberapa film yang mana untuk sekedar menghibur dan menyegarkan otak. Dari kisah tersebutlah saya dapat katakan bahwasannya semua tidak perlu untuk dibawa susah, apapun situasi dan kondisi yang ada tetaplah semangat dan jangan lupa berdo'a pada sang pencipta ;)  </font><br>
+      <font class="justify" size="4", face="Times New Roman"> Foto tersebut merupakan suatu gambaran saya selama melakukan Study From Home, Adapun yang saya lakukan di sela-sela waktu ketika sedang mengerjakan tugas ataupun belajar di rumah yaitu mendengarkan lagu dan segelas kopi yang setia menemani saya ketika sedang kuliah daring ataupun sedang nugas. Kedua hal tersebut sepertinya sudah menjadi kebiasaan bagi saya dalam mensiasati rasa jenuh ketika saya hanya diam dirumah, selain itu saya juga kadang menonton beberapa film yang mana untuk sekedar menghibur dan menyegarkan otak. Dari kisah tersebutlah saya dapat katakan bahwasannya semua tidak perlu untuk dibawa susah, apapun situasi dan kondisi yang ada tetaplah semangat dan jangan lupa berdo'a pada sang pencipta ;)  </font><br>
     </div>
   </div>
 </div>
